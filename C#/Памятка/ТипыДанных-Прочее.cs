d88f6455bd8d65d9e671d1485b2d4ba3bd9e5dea
Nikolay Repin <84241491+pleaonme@users.noreply.github.com>
2025-07-12 16:03:44 +0500

    Update ТипыДанных-Прочее.cs

--- C#/Памятка/ТипыДанных-Прочее.cs
+++ C#/Памятка/ТипыДанных-Прочее.cs
@@ -112,30 +112,6 @@ internal class Prompt
 
         }
 
-        // List
-        { 
-            List<int> exampleList = new List<int>(); // Инициализация списка с неопредленным количеством элементов с типом int
-
-            exampleList.Add(13); // Добавляем нвоый элемент в список
-            exampleList.Add(125);
-            exampleList.Add(25);
-            
-            exampleList.Remove(13); // Удаляем элемент из списка по его значению
-            exampleList.RemoveAt(0); // Удаляем элемент из списка по его индексу
-            
-            exampleList.Insert(1, 13); // Вставить элемент 13 на индекс 1
-            
-            Console.WriteLine(exampleList.Count); // Count = Length, Выводит количество элементов списка
-            
-            Console.WriteLine(exampleList.Contains(13)); // Проверка на наличие элемента 13. Выводит True/False
-            Console.WriteLine(exampleList.IndexOf(13)); // Выводит индекс элемента 13. Если не находит его, выводит -1
-            
-            Console.WriteLine(exampleList[0]); // Выводит первый элемент списка
-            Console.WriteLine(exampleList[^1]); // Выводит последний элемент списка
-        }
-
-
-
         // Активация языковых стандартов Кириллицы в консоли (если не работает само)
         {
             Console.InputEncoding = Encoding.Unicode;
