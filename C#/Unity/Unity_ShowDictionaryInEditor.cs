5d434ff0230e34dc21474568faddfbaf314adb7c
Nikolay Repin <84241491+pleaonme@users.noreply.github.com>
2025-07-16 20:44:15 +0500

    Update Unity_ShowDictionaryInEditor.cs

--- C#/Unity/Unity_ShowDictionaryInEditor.cs
+++ C#/Unity/Unity_ShowDictionaryInEditor.cs
@@ -1,1 +1,18 @@
+// Source of code to make Dictionary visible in Unity Editor
+private enum ResourceType {
+  Wood,
+  iron,
+  Gold,
+  Other
+}
+
+[SerializeField] private Dictionary<ResourceType, int> resourceTypeAmountDictionary; // Doesn't work as expected - Dictionary is still unvisible in Editor
+
+[Serializable]
+public Class ResourceTypeAmount {
+  public ResourceType resourceType;
+  public int amount
+}
+
+[SerializeField] private List<ResourceTypeAmount> resourceTypeAmountList;
 
